58c2f3914b2a331a13f61101778c260f87a4a7ea
Zofia Nora <sophienora92@gmail.com>
2025-03-07 08:18:36 +0100

    added small content to the components files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer>
+        <p>© 2025 My Website. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+      <main>
+        <p>This is a simple React website built with Vite.</p>
+        <button onClick={() => alert("You clicked me!")}>Click Me</button>
+      </main>
+    );
+  }
+  
+  export default Main;
